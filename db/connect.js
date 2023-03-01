e54aa6cac2e73e0a6818d725dec8316f58445145
cmacspeedo <cmacspeedo101@gmail.com>
2023-02-28 17:41:13 -0700

    final commit

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
+const connectDB = () => {
+  return mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-}
+  });
+};
 
-module.exports = connectDB
+module.exports = connectDB;
